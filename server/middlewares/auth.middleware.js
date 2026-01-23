f77b5fdb129c89c2a7bdb40310e6f526ae3cc9e3
Manos <manosnikitakis3@gmail.com>
2026-01-23 12:17:02 +0200

    added middleware for admin verification

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -7,16 +7,29 @@ export const verifyToken = (req, res, next) => {
 
   if (!token) {
     return res.status(401).json({ message: "No token provided." });
+    console.log("No token provided.")
   }
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
   if (err) {
     return res.status(403).json({ message: "Invalid or expired token." });
+
   }
 
-  console.log("Decoded JWT payload:", decoded); 
+
   req.user = decoded;
   next();
   });
 };
+
+export const verifyAdmin = (req, res, next) => {
+  if (!req.user){
+    return res.status(401).json({message: "Unauthorized!"})
+  }
+  if (req.user.role !== "Admin"){
+    return res.status(403).json({message: "Access denied. Admins only."})
+  }
+
+  next();
+}
 
