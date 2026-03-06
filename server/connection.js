637b89b5d270e917f6591b3db066a87514846e6b
Manos <manosnikitakis3@gmail.com>
2026-03-06 18:07:59 +0200

    fixed url

--- server/connection.js
+++ server/connection.js
@@ -6,7 +6,7 @@ const { Client } = pg;
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // required on Render
+  ssl: { rejectUnauthorized: false }, 
 });
 
 export { client };
